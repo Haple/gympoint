6408c5b5fb873f33667ddf7137ac3b12c1f6cde0
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-12-08 01:47:55 -0300

    Criada tabela help_orders no banco
    Signed-off-by: Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>

--- src/database/migrations/20191208023652-create-help-orders.js
+++ src/database/migrations/20191208023652-create-help-orders.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('enrollments', {
+    return queryInterface.createTable('help_orders', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -12,30 +12,19 @@ module.exports = {
         references: { model: 'students', key: 'id' },
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
-        allowNull: false,
-      },
-      plan_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'plans', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: false,
-      },
-      start_date: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      end_date: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      canceled_at: {
-        type: Sequelize.DATE,
         allowNull: true,
       },
-      price: {
-        type: Sequelize.DECIMAL,
+      question: {
+        type: Sequelize.TEXT,
         allowNull: false,
+      },
+      answer: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      answer_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
       },
       created_at: {
         type: Sequelize.DATE,
@@ -49,6 +38,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('enrollments');
+    return queryInterface.dropTable('help_orders');
   },
 };
